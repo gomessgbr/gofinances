4d7b6a1696aef250d2ac66698b35ccafd1981311
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-05-08 07:49:41 -0300

    add theme provider and theme on dashboard

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Dashboard } from "./src/screens/Dashboard";
+
 import { ThemeProvider } from "styled-components";
 
-export default function App() {
-  return <Dashboard />;
-}
 
-const styles = StyleSheet.create({});
+import { Dashboard } from "./src/screens/Dashboard";
+import theme from './src/global/styles/theme'
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  );
+}
