9fc30ca04ae5db4781aaa6ca7fc394ebf8605378
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-05-11 16:01:21 -0300

    add new components on dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,27 @@
 import React from "react";
 import { Text, View, StyleSheet } from "react-native";
-import { Container, Header } from "./styles";
+import {
+  Container,
+  Header,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+} from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
-      <Header></Header>
+      <Header>
+        <UserInfo>
+          <Photo />
+          <User>
+            <UserGreeting>Olá</UserGreeting>
+            <UserName>Gabriel</UserName>
+          </User>
+        </UserInfo>
+      </Header>
     </Container>
   );
 }
