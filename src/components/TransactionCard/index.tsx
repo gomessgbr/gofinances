373e83b59766a1093539ed96acc98ccce6abf4ed
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-09-18 18:53:40 -0300

    feat: create new component transactionCard

---
+++ src/components/TransactionCard/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+
+export function TransactionCard(){
+    return(
+        <Container>
+            
+        </Container>
+    )
+}
