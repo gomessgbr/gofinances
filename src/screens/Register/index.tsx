c3f1341b1d733e74f67c6f480e50e968f4c458eb
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-09-25 16:52:31 -0300

    feat: create register screen

---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+import { Container } from "./styles";
+
+export default function Register() {
+  return (
+    
+      <Container>
+        
+      </Container>
+    
+  );
+}
