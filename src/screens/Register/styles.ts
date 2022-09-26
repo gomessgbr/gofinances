708592553d97197285ef3ce2d9603ccfe937799c
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-09-26 19:55:15 -0300

    style: styling container

--- src/screens/Register/styles.ts
+++ src/screens/Register/styles.ts
@@ -1,7 +1,10 @@
 import styled from "styled-components/native";
 import { RFValue } from "react-native-responsive-fontsize";
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background};
+`;
 
 export const Header = styled.View`
   background-color: ${({ theme }) => theme.colors.primary};
