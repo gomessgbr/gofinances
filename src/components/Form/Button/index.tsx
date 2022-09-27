c1e14c4ba1897963ed5853ddcb1873ddcdd2a1e2
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-09-27 11:04:39 -0300

    feat: create Button Component

---
+++ src/components/Form/Button/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Title } from "./styles";
+
+export function Button() {
+  return (
+    <>
+      <Container>
+        <Title>Zica</Title>
+      </Container>
+    </>
+  );
+}
