0ad9602a18e2559176cfb1599af0c8aba3f735d6
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-09-28 18:05:37 -0300

    style: padding, align and border-radius on button

--- src/components/Form/Button/styles.ts
+++ src/components/Form/Button/styles.ts
@@ -4,11 +4,13 @@ import styled from "styled-components/native";
 export const Container = styled.TouchableOpacity`
   width: 100%;
   background-color: ${({ theme }) => theme.colors.secundary};
+  border-radius: 5px;
+  align-items: center;
+  padding: 18px;
 `;
 
 export const Title = styled.Text`
   font-family: ${({ theme }) => theme.fonts.medium};
   font-size: ${RFValue(14)}px;
   color: ${({ theme }) => theme.colors.shape};
-  padding: 18px;
 `;
