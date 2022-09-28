cac0150c607f784ca443eafb3001ff9a3c52d571
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-09-28 19:26:55 -0300

    feat: create variable for choice icon

--- src/components/Form/TransactionTypeButton/index.tsx
+++ src/components/Form/TransactionTypeButton/index.tsx
@@ -4,15 +4,20 @@ import { Container, Title, Icon } from "./styles";
 
 import { TouchableOpacityProps } from "react-native";
 
+const icons = {
+  up: "arrow-up-circle",
+  down: "arrow-down-circle",
+};
+
 interface Props extends TouchableOpacityProps {
   title: string;
   type: "up" | "down";
 }
 
-export function TransactionTypeButton({ title, ...rest }: Props) {
+export function TransactionTypeButton({ title, type, ...rest }: Props) {
   return (
     <Container {...rest}>
-      <Icon />
+      <Icon name={icons[type]} />
       <Title>{title}</Title>
     </Container>
   );
