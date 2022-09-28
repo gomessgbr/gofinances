b7b2d103a83d9a6ae9a639a5aeeb639a175c5153
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-09-28 20:08:22 -0300

    style: add backgrounds colors on buttons clicked

--- src/components/Form/TransactionTypeButton/styles.ts
+++ src/components/Form/TransactionTypeButton/styles.ts
@@ -1,21 +1,41 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 import { Feather } from "@expo/vector-icons";
 import { RFValue } from "react-native-responsive-fontsize";
 
-interface TransactionProps {
+interface IconProps {
   type: "up" | "down";
 }
 
-export const Container = styled.TouchableOpacity`
+interface ContainerProps {
+  isActive: boolean;
+  type: "up" | "down";
+}
+
+export const Container = styled.TouchableOpacity<ContainerProps>`
   width: 48%;
   flex-direction: row;
   align-items: center;
   justify-content: center;
-  border: 1.5px solid ${({ theme }) => theme.colors.text};
+  border-width: ${({ isActive }) => (isActive ? 0 : 1.5)}px;
+  border-style: solid;
+  border-color: ${({ theme }) => theme.colors.text};
+  border-radius: 5px;
   padding: 16px;
+  ${({ isActive, type }) =>
+    isActive &&
+    type === "up" &&
+    css`
+      background-color: ${({ theme }) => theme.colors.success_light};
+    `}
+  ${({ isActive, type }) =>
+    isActive &&
+    type === "down" &&
+    css`
+      background-color: ${({ theme }) => theme.colors.attention_light};
+    `}
 `;
 
-export const Icon = styled(Feather)<TransactionProps>`
+export const Icon = styled(Feather)<IconProps>`
   font-size: ${RFValue(24)}px;
   margin-right: 12px;
 
