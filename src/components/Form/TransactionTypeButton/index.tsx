6a3c0d50e643b62538ed1f76a4cbcebb323ec019
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-09-28 19:15:02 -0300

    feat: create TransactionTypeButton Component

---
+++ src/components/Form/TransactionTypeButton/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Container, Zica } from "./styles";
+
+export function TransactionTypeButton() {
+  return (
+    <Container>
+      <Zica></Zica>
+    </Container>
+  );
+}
