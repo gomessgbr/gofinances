a0f91d0de01597a91e3f7d767c23073716d96146
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-09-29 17:07:02 -0300

    style

--- src/screens/CategorySelect/styles.ts
+++ src/screens/CategorySelect/styles.ts
@@ -1,7 +1,22 @@
+import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background};
+`;
 
-export const Title = styled.Text``;
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFValue(113)}px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  align-items: center;
+  justify-content: flex-end;
+  padding-bottom: 19px;
+`;
 
-export const Header = styled.View``;
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(18)}px;
+  color: ${({ theme }) => theme.colors.shape};
+`;
