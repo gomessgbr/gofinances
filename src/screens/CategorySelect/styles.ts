5271138d693dc6af0cb9be1f294737876eb4342f
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-09-29 17:20:30 -0300

    style

--- src/screens/CategorySelect/styles.ts
+++ src/screens/CategorySelect/styles.ts
@@ -23,8 +23,25 @@ export const Title = styled.Text`
   color: ${({ theme }) => theme.colors.shape};
 `;
 
-export const Category = styled.View``;
+export const Category = styled.View`
+  width: 100%;
+  padding: ${RFValue(15)}px;
+  flex-direction: row;
+  align-items: center;
+`;
 
-export const Icon = styled(Feather)``;
+export const Icon = styled(Feather)`
+  font-size: ${RFValue(20)}px;
+  margin-right: 16px;
+`;
 
-export const Name = styled.View``;
+export const Name = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(14)}px;
+`;
+
+export const Separator = styled.View`
+  height: 1px;
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.text};
+`;
