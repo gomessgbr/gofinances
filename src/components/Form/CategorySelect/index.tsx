62e792d3cd28ac7de155dee8f34f1dfc7ecd9ba6
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-09-29 16:04:52 -0300

    feat: CreateCategorySelect

---
+++ src/components/Form/CategorySelect/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Container, Title } from "./styles";
+
+export function CategorySelect() {
+  return (
+    <Container>
+      <Title></Title>
+    </Container>
+  );
+}
