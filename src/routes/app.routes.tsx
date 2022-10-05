dc93858890a70be7c76f82f3c394fc869524c9fe
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-10-05 19:28:38 -0300

    feat: add props

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
+
+
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export function AppRoutes() {
   return (
     <Navigator>
-      <Screen />
+      <Screen  name="Listagem" component={}/>
     </Navigator>
   );
 }
