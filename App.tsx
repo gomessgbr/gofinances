b670322c759a429ff14b51ffd9c799b8f221ab14
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-10-05 19:33:00 -0300

    feat: add routes navigation on  root

--- App.tsx
+++ App.tsx
@@ -13,6 +13,9 @@ import { Register } from "./src/screens/Register";
 import { CategorySelect } from "./src/screens/CategorySelect";
 import theme from "./src/global/styles/theme";
 
+import { AppRoutes } from "./src/routes/app.routes";
+import { NavigationContainer } from "@react-navigation/native";
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
@@ -25,7 +28,9 @@ export default function App() {
   }
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
