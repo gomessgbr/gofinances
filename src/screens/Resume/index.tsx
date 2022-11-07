acd1022042158da881d746c81635e296d2e1733a
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-11-07 20:09:17 -0300

    feat: function for sum categories

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,17 +1,54 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { HistoryCard } from "../../components/HistoryCard";
 
 import { Container, Header, Title } from "./styles";
+import { categories } from "../../utils/categories";
+
+interface TransactionData {
+  type: "positive" | "negative";
+  name: string;
+  amount: string;
+  category: string;
+  date: string;
+}
 
 export function Resume() {
   async function loadData() {
     const dataKey = "@gofinance: transactions";
-    const data = await AsyncStorage.getItem(dataKey);
-    const currentData = data ? JSON.parse(data) : [];
+    const response = await AsyncStorage.getItem(dataKey);
+    const responseFormatted = response ? JSON.parse(response) : [];
+
+    const expensive = responseFormatted.filter(
+      (expensive: TransactionData) => expensive.type === "negative"
+    );
+
+    const totalByCategory = [];
+
+    categories.forEach((category) => {
+      let categorySum = 0;
+
+      expensive.forEach((expensive: TransactionData) => {
+        if (expensive.category === category.key) {
+          categorySum += Number(expensive.amount);
+        }
+      });
+
+      if (categorySum > 0) {
+        totalByCategory.push({
+          name: category.name,
+          total: categorySum,
+        });
+      }
+    });
+    console.log(totalByCategory);
   }
+
+  useEffect(() => {
+    loadData();
+  }, []);
 
   return (
     <Container>
