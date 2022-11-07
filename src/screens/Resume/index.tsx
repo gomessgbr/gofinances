bc06267fec73c331aec49c1b03bd58c04886e160
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-11-07 19:55:37 -0300

    feat: fetch transaction on asyncstorage

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import { HistoryCard } from "../../components/HistoryCard";
 
 import { Container, Header, Title } from "./styles";
 
 export function Resume() {
+  async function loadData() {
+    const dataKey = "@gofinance: transactions";
+    const data = await AsyncStorage.getItem(dataKey);
+    const currentData = data ? JSON.parse(data) : [];
+  }
+
   return (
     <Container>
       <Header>
