98d71c718e2d5b680b2363d9ae430f01ddde6dde
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-11-08 16:18:08 -0300

    feat: light status bar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { StatusBar } from "react-native";
+
 import "intl";
 import "intl/locale-data/jsonp/pt-BR";
 
@@ -31,6 +33,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
+        <StatusBar barStyle="light-content" />
         <AppRoutes />
       </NavigationContainer>
     </ThemeProvider>
