5a66efe803720b9b1311286d8243f51a5156bb1e
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-11-10 19:13:20 -0300

    feat: new styled components for signin

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { RFValue } from "react-native-responsive-fontsize";
 
-import { Container } from "./styles";
+import AppleSVG from "../../assets/apple.svg";
+import GoogleSVG from "../../assets/google.svg";
+import LogoSVG from "../../assets/logo.svg";
+
+import {
+  Container,
+  Header,
+  TitleWrapper,
+  Title,
+  SignInTitle,
+  Footer,
+} from "./styles";
 
 export function SignIn() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Header>
+        <TitleWrapper>
+          <LogoSVG width={RFValue(120)} height={68} />
+          <Title>
+            Controle suas {"\n"}
+            finanças de forma {"\n"}
+            muito simples
+          </Title>
+        </TitleWrapper>
+        <SignInTitle>
+          Faça seu login com {"\n"}
+          uma conta abaixo
+        </SignInTitle>
+      </Header>
+      <Footer></Footer>
+    </Container>
+  );
 }
