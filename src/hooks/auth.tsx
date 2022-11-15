30965807c460fac0d3ce98dd7c701668dca03311
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-11-14 21:39:30 -0300

    feat: const for auth

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,5 +1,7 @@
 import React, { createContext, ReactNode, useContext } from "react";
 
+import * as AuthSession from "expo-auth-session";
+
 interface AuthProviderProps {
   children: ReactNode;
 }
@@ -13,6 +15,7 @@ interface User {
 
 interface AuthContextData {
   user: User;
+  signInWithGoogle(): Promise<void>;
 }
 
 const AuthContext = createContext({} as AuthContextData);
@@ -26,10 +29,15 @@ function AuthProvider({ children }: AuthProviderProps) {
 
   async function signInWithGoogle() {
     try {
-      const CLIENT_ID = "";
-      const REDIRECT_URI = "";
-      const RESPONSE_TYPE = "";
-      const SCOPE = "";
+      const CLIENT_ID =
+        "563445416712-gllsck9kod84qqblgpsii29lo7h80q17.apps.googleusercontent.com";
+      const REDIRECT_URI = "https://auth.expo.io/@gomessgbr/gofinances";
+      const RESPONSE_TYPE = "token";
+      const SCOPE = encodeURI("profile email");
+
+      const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+      const response = await AuthSession.startAsync({ authUrl });
+      console.log("response", response);
     } catch (error) {
       throw new Error(error);
     }
@@ -39,6 +47,7 @@ function AuthProvider({ children }: AuthProviderProps) {
     <AuthContext.Provider
       value={{
         user,
+        signInWithGoogle,
       }}
     >
       {children}
